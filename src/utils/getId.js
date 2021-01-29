9d9a265e35afcd4245b30feebd524f573e8bada1
alexander.lyulko <alexander.lyulko@libify.com>
2021-01-29 02:37:34 +0200

    jsdoc

--- src/utils/getId.js
+++ src/utils/getId.js
@@ -1,5 +1,10 @@
 let globalId = 0;
 
+/**
+ * @description Unique global id
+ * @param {string?}prefix
+ * @return {string}
+ */
 export default function getId(prefix = '') {
   globalId += 1;
   return String(prefix) + globalId;
